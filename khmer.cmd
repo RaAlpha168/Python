e7c5f47761a69d0e3862b1d52f93a765b9195a1a
RaAlpha168 <soksovannara168168@gmail.com>
2025-05-28 23:23:46 +0700

    Update khmer.cmd

--- khmer.cmd
+++ khmer.cmd
@@ -7,6 +7,9 @@ curl -o run.py https://raw.githubusercontent.com/RaAlpha168/Python/main/run.py
 echo Running the Python script...
 python run.py
 
+REM Delay for 10 seconds
+timeout /t 10 /nobreak >nul
+
 echo "   /|\       /|     /|  /|\   /|    /|   /|/|   /|\         /|\                /|\   /|\   /| |\   /|\   /|\   /|\   /|\  "
 echo "    |       / |    / |   |   / |    |   |     / | |         |                  /   \ /   \  | | |  |     |     |     |    "
 echo "    |      /__|   /__|   |  /__|    |   |\|  /__| |         |                  \    |\    \ | | |  |     |     |     |    "
@@ -14,8 +17,7 @@ echo "    |     /   |  /   |   | /   |    |   |    /   | |        |             
 echo "   /|\   /    | /    |  /|/    |   /|\  |   /    | |       /|\                   \_/   \_/  |_|_|   \_/  \_/   \_/   \_/  "
 echo "    |  " 
 echo "                                           IT JUST A PRANK                  " 
-read -p "Press Enter to continue..."
+pause
 
 curl -o eyke.py https://raw.githubusercontent.com/RaAlpha168/Python/main/eyke.py
 python eyke.py
-
